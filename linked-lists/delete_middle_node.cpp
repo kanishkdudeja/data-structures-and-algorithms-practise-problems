20051c6a0bb863c1efa152b848199d84c2a39d07
Kanishk Dudeja <kanishk.dudeja@wingify.com>
2017-06-03 01:08:05 +0530

    Added solution for deleting middle node of linked list

---
+++ linked-lists/delete_middle_node.cpp
@@ -0,0 +1,60 @@
+//Complexity = O(n) or 0(1) if we assume that the function can only iterate
+//255 times at maximum to find out an answer
+
+#include <iostream>
+using namespace std;
+
+struct Node {
+	int data = 0;
+	Node* next = nullptr;
+};
+
+Node* insertNodeAtHead(Node* head, int data) {
+	Node* newNode = new Node;
+	newNode->data = data;
+	newNode->next = head;
+
+	return newNode;
+}
+
+void printLinkedList(Node* head) {
+	Node* tmpNode = head;
+	while(tmpNode->next) {
+		cout<<tmpNode->data<<"\n";
+		tmpNode = tmpNode->next;
+	}
+	cout<<tmpNode->data<<"\n";
+}
+
+void deleteMiddleNode(Node* middle) {
+	Node* next;
+	next = middle->next;
+
+	middle->data = next->data;
+	middle->next = next->next;
+
+	//free(next);
+}
+
+int main ()
+{
+	Node* head = NULL;
+
+	head = insertNodeAtHead(head, 0);
+	head = insertNodeAtHead(head, 1);
+	head = insertNodeAtHead(head, 2);
+	head = insertNodeAtHead(head, 3);
+	head = insertNodeAtHead(head, 4);
+	head = insertNodeAtHead(head, 2);
+	head = insertNodeAtHead(head, 3);
+	head = insertNodeAtHead(head, 4);
+	head = insertNodeAtHead(head, 5);
+	
+	cout<<"Original Linked List: "<<"\n";
+	printLinkedList(head);
+
+	deleteMiddleNode(head->next);
+
+	cout<<"Linked list after removing 2nd(middle) element: "<<"\n";
+	printLinkedList(head);
+}
