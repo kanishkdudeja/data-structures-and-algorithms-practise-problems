f0014c291a7b05b92f3c24d83e6b72b8318f1dd8
Kanishk Dudeja <kanishk.dudeja@wingify.com>
2017-07-25 12:59:34 +0530

    Added potential solutions to find first repeating element

---
+++ find-first-repeating-element.cpp
@@ -0,0 +1,17 @@
+/* either use sorting O(nlog(n))
+or use comparisons with each one O(n2)
+or use hashing O(n)
+*/
+
+#include <iostream>
+using namespace std;
+
+int findFirstRepeatingElement(int* arr, int size) {
+
+}
+
+int main() {
+	int arr[10] = { 1, 2, 3, 4, 5, 7, 2, 8, 9, 10 };
+
+	cout<<findFirstRepeatingElement(arr, 10);
+}
