9e9a89c49be0825fe56ffe4da2c1aa617f93897f
Kanishk Dudeja <kanishk.dudeja@wingify.com>
2017-08-16 00:23:49 +0530

    Added solution to find minimum depth of a binary tree

--- trees-and-graphs/minimum-depth-binary-tree.cpp
+++ trees-and-graphs/minimum-depth-binary-tree.cpp
@@ -1,17 +1,17 @@
-/* Write a program to find the maximum depth (height) of a binary tree
+/* Write a program to find the minimum depth of a binary tree
 
-maxDepth()
+minDepth()
 1. If tree is empty then return 0
 2. Else
-     (a) Get the max depth of left subtree recursively  i.e., 
-          call maxDepth( tree->left-subtree)
-     (a) Get the max depth of right subtree recursively  i.e., 
-          call maxDepth( tree->right-subtree)
-     (c) Get the max of max depths of left and right 
+     (a) Get the min depth of left subtree recursively  i.e., 
+          call minDepth( tree->left-subtree)
+     (a) Get the min depth of right subtree recursively  i.e., 
+          call minDepth( tree->right-subtree)
+     (c) Get the min of min depths of left and right 
           subtrees and add 1 to it for the current node.
-         max_depth = max(max dept of left subtree, max depth of right subtree) + 1
-     (d) Return max_depth
- 
+         min_depth = min(min dept of left subtreete, min depth of right subtree) + 1
+     (d) Return min_depth
+
 */
 
 #include <iostream>
@@ -45,19 +45,19 @@ TreeNode* insertIntoBST(TreeNode* root, int data) {
  	return root;
 }
 
-int getMaximumDepth(TreeNode* root) {
+int getMinimumDepth(TreeNode* root) {
 	if(root == NULL) {
 		return 0;
 	}
 	else {
 		if(root->left != NULL && root->right != NULL) {
-			return 1 + max(getMaximumDepth(root->left), getMaximumDepth(root->right));
+			return 1 + min(getMinimumDepth(root->left), getMinimumDepth(root->right));
 		}
 		else if(root->left == NULL) {
-			return 1 + getMaximumDepth(root->right);
+			return 1 + getMinimumDepth(root->right);
 		}
 		else if(root->right == NULL) {
-			return 1 + getMaximumDepth(root->left);
+			return 1 + getMinimumDepth(root->left);
 		}
 	}
 }
@@ -69,9 +69,7 @@ int main() {
 	root = insertIntoBST(root, 3);
 	root = insertIntoBST(root, 5);
 	root = insertIntoBST(root, 2);
-	root = insertIntoBST(root, 7);
 	root = insertIntoBST(root, 1);
-	root = insertIntoBST(root, 6);
-
-	cout<<getMaximumDepth(root);
+	
+	cout<<getMinimumDepth(root);
 }
