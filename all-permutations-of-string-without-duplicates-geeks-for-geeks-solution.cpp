be8047cb1ccc7204fb2fb9bb4c93770cac8a4af7
Kanishk Dudeja <kanishk.dudeja@wingify.com>
2017-08-31 00:51:00 +0530

    Added solution to find all permutations of a string (by geeksforgeeks)

---
+++ all-permutations-of-string-without-duplicates-geeks-for-geeks-solution.cpp
@@ -0,0 +1,52 @@
+/*
+
+A permutation, also called an “arrangement number” or “order,” is a rearrangement of the elements
+of an ordered list S into a one-to-one correspondence with S itself. 
+
+A string of length n has n! permutations.
+
+Below are the permutations of string ABC.
+ABC ACB BAC BCA CBA CAB
+
+Here is a solution that is used as a basis in backtracking.
+
+http://www.geeksforgeeks.org/wp-content/uploads/NewPermutation.gif
+
+Algorithm Paradigm: Backtracking
+Time Complexity: O(n*n!) Note that there are n! permutations and it requires O(n) time to print a a permutation.
+
+Note : The above solution prints duplicate permutations if there are repeating characters in input string.
+
+*/
+
+#include <iostream>
+using namespace std;
+
+void swap(char* a, char* b) {
+	char temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+void printPermutations(char* str, int start, int end) {
+	int i;
+
+	if(start == end) {
+		cout<<str<<endl;
+	}
+	else {
+		for(i = start; i <= end; i++) {
+			swap(str+start, str+i);
+
+			printPermutations(str, start+1, end);
+
+			swap(str+start, str+i);
+		}
+	}
+}
+
+int main() {
+	char s[4] = {'a', 'b', 'c'};
+
+	printPermutations(s, 0, 2);
+}
