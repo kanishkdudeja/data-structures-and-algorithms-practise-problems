8c01d2d33d87e70d9815b1db9405fc267f4aca28
Kanishk Dudeja <me@kanishkdudeja.com>
2017-11-04 23:41:12 +0530

    Added linespaces

--- arrays-and-strings/randomly-shuffle-an-array.cpp
+++ arrays-and-strings/randomly-shuffle-an-array.cpp
@@ -12,7 +12,8 @@ Now, sort the elements of the original array in a way that they occur in their i
 Sorting here will require O(nlogn) time and storing an additional auxilliary array will require O(n) space
 
 Fisher–Yates shuffle Algorithm works in O(n) time complexity. The assumption here is, we are given a function rand() that generates random number in O(1) time.
-The idea is to start from the last element, swap it with a randomly selected element from the whole array (including last). Now consider the array from 0 to n-2 (size reduced by 1), and repeat the process till we hit the first element.
+The idea is to start from the last element, swap it with a randomly selected element from the whole array (including last). Now consider the array 
+from 0 to n-2 (size reduced by 1), and repeat the process till we hit the first element.
 
 Following is the detailed algorithm
 
@@ -30,10 +31,15 @@ The probability that ith element (including the last one) goes to last position 
 
 The probability that ith element goes to second last position can be proved to be 1/n by dividing it in two cases.
 Case 1: i = n-1 (index of last element):
-The probability of last element going to second last position is = (probability that last element doesn't stay at its original position) x (probability that the index picked in previous step is picked again so that the last element is swapped)
+The probability of last element going to second last position is = 
+(probability that last element doesn't stay at its original position) x (probability that the index picked in previous step is picked again 
+so that the last element is swapped)
+
 So the probability = ((n-1)/n) x (1/(n-1)) = 1/n
+
 Case 2: 0 < i < n-1 (index of non-last):
-The probability of ith element going to second position = (probability that ith element is not picked in previous iteration) x (probability that ith element is picked in this iteration)
+The probability of ith element going to second position = 
+(probability that ith element is not picked in previous iteration) x (probability that ith element is picked in this iteration)
 So the probability = ((n-1)/n) x (1/(n-1)) = 1/n
 
 */
